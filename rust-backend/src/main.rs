b0f89d137bc6555ff02dd6379cdf2cf240ec497b
Adam Zdrzalka <57966028+moderndayNeo@users.noreply.github.com>
2025-03-01 16:22:44 +0100

    Setup CORS for the rust-backend to receive requests from the FE server

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -1,19 +1,31 @@
+use actix_cors::Cors;
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().json(serde_json::json!({
+        "message": "Hello world!"
+    }))
 }
 
 #[get("/hello/{name}")]
 async fn hello_name(name: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Hello {}!", name))
+    HttpResponse::Ok().json(serde_json::json!({
+        "message": format!("Hello {}!", name)
+    }))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(hello_name))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allow_any_method()
+            .allow_any_header();
+
+        App::new().wrap(cors).service(hello).service(hello_name)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
