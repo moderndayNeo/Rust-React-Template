fb7d4cbd8e8de08f001474310ca7e144cff3cfef
Adam Zdrzalka <57966028+moderndayNeo@users.noreply.github.com>
2025-03-03 20:11:51 +0100

    Update the font-colors for the AiTools components

--- react-vite-frontend/src/components/AiTools.tsx
+++ react-vite-frontend/src/components/AiTools.tsx
@@ -27,15 +27,15 @@ export function AiTools() {
   console.log(data);
 
   return (
-    <div>
-      <h3 className="text-2xl font-bold mb-4">AiTools</h3>
+    <div >
+      <h3 className="text-2xl font-bold mb-4 text-gray-200">AiTools</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {data.tools.map((tool: AiTool) => (
           <div key={tool.name} className="border p-4 rounded-lg shadow-md">
-            <h4 className="text-xl font-semibold mb-2">{tool.name}</h4>
-            <p className="text-gray-700 mb-2">{tool.description}</p>
-            <p className="text-gray-500 mb-2">{tool.company}</p>
-            <p className="text-gray-900 font-bold mb-2">
+            <h4 className="text-xl text-gray-200 font-semibold mb-2">Name: {tool.name}</h4>
+            <p className="text-gray-200 mb-2">Desc.: {tool.description}</p>
+            <p className="text-gray-200 mb-2">Company: {tool.company}</p>
+            <p className="text-gray-200 font-bold mb-2">
               ${tool.monthly_price_usd}/month
             </p>
             {tool.image && (
@@ -48,6 +48,9 @@ export function AiTools() {
           </div>
         ))}
       </div>
+
+
+
     </div>
   );
 }
