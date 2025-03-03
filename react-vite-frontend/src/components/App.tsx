09fbe0f3e6433edee796d0f9198c7c7593370e3d
Adam Zdrzalka <57966028+moderndayNeo@users.noreply.github.com>
2025-03-03 20:11:28 +0100

    Move ExampleDataComponent to its own file

--- react-vite-frontend/src/components/App.tsx
+++ react-vite-frontend/src/components/App.tsx
@@ -5,39 +5,15 @@ import { use, Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { AiTools } from './AiTools';
 
-async function fetchData() {
-  const response = await fetch(import.meta.env.VITE_BACKEND_URL);
-  if (!response.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  const json = await response.json();
-
-  return json;
-}
-
-const promise = fetchData();
-
-const DataComponent: React.FC = () => {
-  const data = use(promise);
-
-  return (
-    <div>
-      <h2>{data.message}</h2>
-    </div>
-  );
-};
-
 const App: React.FC = () => {
   return (
     <div className="p-14">
-      <h1 className="text-4xl text-blue-700">
+      <h1 className="text-4xl text-gray-100">
         Welcome to the Vite + React + TypeScript App!
       </h1>
 
       <ErrorBoundary fallback={<div>Error loading data!</div>}>
         <Suspense fallback={<div>Loading...</div>}>
-          <DataComponent />
           <AiTools />
         </Suspense>
       </ErrorBoundary>
---
+++ react-vite-frontend/src/components/ExampleDataComponent.tsx
@@ -0,0 +1,24 @@
+import { use } from 'react';
+
+async function fetchData() {
+  const response = await fetch(import.meta.env.VITE_BACKEND_URL);
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const json = await response.json();
+
+  return json;
+}
+
+const promise = fetchData();
+
+export const ExampleDataComponent: React.FC = () => {
+  const data = use(promise);
+
+  return (
+    <div>
+      <h2>{data.message}</h2>
+    </div>
+  );
+};
