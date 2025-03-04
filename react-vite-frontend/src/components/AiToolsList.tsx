2d965be968d24358487cd8567479cc0ca529dd4c
Adam Zdrzalka <57966028+moderndayNeo@users.noreply.github.com>
2025-03-04 19:22:39 +0100

    Allow users to filter AI tools by name

--- react-vite-frontend/src/components/AiToolsList.tsx
+++ react-vite-frontend/src/components/AiToolsList.tsx
@@ -1,6 +1,8 @@
-import { use } from 'react';
+import { use, useState } from 'react';
 import { AiToolComponent } from './AiToolComponent';
 import { AiTool } from '@/types';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
 
 async function fetchData() {
   const response = await fetch(import.meta.env.VITE_AI_TOOLS_URL);
@@ -15,12 +17,25 @@ const promise = fetchData();
 
 export function AiToolsList() {
   const aiToolsData = use(promise);
+  const [filterString, setFilterString] = useState('');
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {aiToolsData.tools.map((tool: AiTool) => (
-        <AiToolComponent tool={tool} key={tool.id} />
-      ))}
+      <Label className="text-gray-200">
+        Filter by name:
+        <Input
+          value={filterString}
+          onChange={(e) => setFilterString(e.target.value)}
+        />
+      </Label>
+
+      {aiToolsData.tools
+        .filter((tool: AiTool) =>
+          tool.name.toLowerCase().includes(filterString.toLowerCase())
+        )
+        .map((tool: AiTool) => (
+          <AiToolComponent tool={tool} key={tool.id} />
+        ))}
     </div>
   );
 }
